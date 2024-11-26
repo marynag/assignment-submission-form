05368d35b9f16f43719471f5d9f22353a77d3fe0
marynag <mgrabchak1109@gmail.com>
2024-11-26 16:57:19 +0200

    fix the thank you page text

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
       <IconButton className={"md:h-10 md:w-10 h-6 w-6"} />
       <h1 className="text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl md:mt-12">
         <span className="text-transparent bg-clip-text bg-gradient-to-r  from-lime-300 via-lime-400 to-lime-500">
-          Thank you
+          Thank you for submitting your assignment!
         </span>
       </h1>
     </div>
